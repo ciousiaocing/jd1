f1d4d75876c6d25d8c0c649598894b310971fb6d
ciou <yyouko22@gmail.com>
2017-12-19 21:28:28 +0800

    admin/orders/show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,13 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_required
+
   def index
     @orders = Order.all
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+    @product_lists = @order.product_lists
   end
 end
