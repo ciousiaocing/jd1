ba35512d2abf45bdce05d028edf586525cdb5dee
ciou <yyouko22@gmail.com>
2017-12-20 10:11:19 +0800

    fix orders_controler checkoutwithbraintree bug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -44,8 +44,9 @@ class OrdersController < ApplicationController
       )
 
       if result
-        redirect_to order_path(@order.token), notice: "braintree完成付款"
         @order.make_payment!
+
+        redirect_to order_path(@order.token), notice: "braintree完成付款"
         current_cart.clean!
 
         OrderMailer.notify_order_placed(@order).deliver_now
